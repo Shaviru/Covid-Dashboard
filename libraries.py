b3d6bc312d6d35c3898ee8288cc24ac714e08509
Shaviru <93417410+Shaviru@users.noreply.github.com>
2021-11-27 11:32:59 +0530

    Update libraries.py

--- libraries.py
+++ libraries.py
@@ -21,16 +21,6 @@ def get_historic_deaths(country, days):
 
   return deaths_df
 
-def get_historic_recoveries(country, days):
-
-  resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays={days}')
-  data = resp.json()
-  recoveries_df = pd.DataFrame(data=data['timeline']['recovered'].values(), 
-                      index=data['timeline']['recovered'].keys(),
-                      columns=['recovered'])
-
-  return recoveries_df
-
 def get_yesterday_cases(country):
 
   resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays=1')
@@ -44,14 +34,6 @@ def get_yesterday_deaths(country):
   resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays=1')
   data = resp.json()
   count = list(data['timeline']['deaths'].values())[0]
-
-  return count 
-
-def get_yesterday_recoveries(country):
-
-  resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays=1')
-  data = resp.json()
-  count = list(data['timeline']['recovered'].values())[0]
 
   return count 
 
@@ -72,14 +54,5 @@ def get_daily_deaths(country,days):
     cumilative = pd.DataFrame(data=data['timeline']['deaths'].values(), 
                     index=data['timeline']['deaths'].keys(),
                     columns=['deaths'])
-    daily = cumilative.diff().fillna(0)
-    return daily 
-
-def get_daily_recoveries(country,days):
-    resp = requests.get(f'https://disease.sh/v2/historical/{country}?lastdays={days}')
-    data = resp.json()
-    cumilative = pd.DataFrame(data=data['timeline']['recovered'].values(), 
-                    index=data['timeline']['recovered'].keys(),
-                    columns=['recovered'])
     daily = cumilative.diff().fillna(0)
     return daily 
